c405a579ce69ddb77d8c8358b44daca326ed6a91
Martin Wencl <marta.wencl@gmail.com>
2024-10-20 20:21:13 +0200

    Nvim - add first mini plugins

---
+++ usr/nvim/lua/plugins/mini.lua
@@ -0,0 +1,45 @@
+return {
+    {
+        "echasnovski/mini.pairs",
+        version = false,
+        config = function()
+            require('mini.pairs').setup()
+        end,
+    },
+    {
+        "echasnovski/mini.ai",
+        version = false,
+        config = function()
+            require('mini.ai').setup()
+        end,
+    },
+    {
+        "echasnovski/mini.surround",
+        version = false,
+        config = function()
+            require('mini.surround').setup()
+        end,
+    },
+    {
+        "echasnovski/mini.operators",
+        version = false,
+        config = function()
+            require('mini.operators').setup()
+        end,
+    },
+    {
+        "echasnovski/mini.bracketed",
+        version = false,
+        config = function()
+            require('mini.bracketed').setup()
+        end,
+    },
+    {
+        "echasnovski/mini.jump2d",
+        version = false,
+        config = function()
+            require('mini.jump2d').setup()
+        end,
+    },
+}
+
