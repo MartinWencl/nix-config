b5a7da5b4588392f166700f979821e0c3516dd7f
Martin Wencl <marta.wencl@gmail.com>
2025-04-05 22:49:22 +0200

    home-manager: update to derivation drvhash:0:7

--- usr/nvim/lua/plugins/none_ls.lua
+++ usr/nvim/lua/plugins/none_ls.lua
@@ -2,22 +2,22 @@
 -- NOTE: LSP wrapper for command line Utilities
 -- can be used for linters and formatter
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-
-    null_ls.setup {
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
-      },
-    }
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-
-        -- Create a command `:Format` local to the LSP buffer
-        vim.api.nvim_buf_create_user_command(0, "Format", function(_)
-            vim.lsp.buf.format()
-        end, { desc = "format current buffer with lsp" })
-    end,
+    -- "nvimtools/none-ls.nvim",
+    -- config = function()
+    --     local null_ls = require("null-ls")
+    --
+    -- null_ls.setup {
+    --   sources = {
+    --     null_ls.builtins.formatting.stylua,
+    --     -- null_ls.builtins.diagnostics.eslint,
+    --     null_ls.builtins.completion.spell,
+    --   },
+    -- }
+    --     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    --
+    --     -- Create a command `:Format` local to the LSP buffer
+    --     vim.api.nvim_buf_create_user_command(0, "Format", function(_)
+    --         vim.lsp.buf.format()
+    --     end, { desc = "format current buffer with lsp" })
+    -- end,
 }
