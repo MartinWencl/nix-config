214a45b2b3773e9e918e2d42dfe1b9eafcb6ac1a
Martin Wencl <marta.wencl@gmail.com>
2026-02-17 20:57:18 +0100

    home-manager: update to derivation nix

--- usr/nvim/after/lsp/omnisharp.lua
+++ usr/nvim/after/lsp/omnisharp.lua
@@ -4,24 +4,26 @@ local util = require('lspconfig.util')
 return {
     root_dir = function(bufnr, on_dir)
         local fname = vim.api.nvim_buf_get_name(bufnr)
+        vim.notify('omnisharp root_dir: fname=' .. fname, vim.log.levels.DEBUG)
 
-        -- First, try to find a .sln file by searching upward
         local sln_root = util.root_pattern('*.sln', '*.slnx')(fname)
         if sln_root then
+            vim.notify('omnisharp root_dir: sln_root=' .. sln_root, vim.log.levels.DEBUG)
             return on_dir(sln_root)
         end
 
-        -- If no .sln found, check if we're in a monorepo structure
-        -- Search upward for .git, then look for .sln in immediate subdirectories
         local git_root = util.root_pattern('.git')(fname)
+        vim.notify('omnisharp root_dir: git_root=' .. tostring(git_root), vim.log.levels.DEBUG)
         if git_root then
             local sln_files = vim.fn.glob(git_root .. '/*/*.sln', false, true)
+            vim.notify('omnisharp root_dir: sln_files=' .. #sln_files, vim.log.levels.DEBUG)
             if #sln_files > 0 then
                 return on_dir(git_root)
             end
         end
 
-        -- Fall back to .csproj as project root
-        on_dir(util.root_pattern('*.csproj', 'omnisharp.json', 'function.json')(fname) or git_root)
+        local fallback = util.root_pattern('*.csproj', 'omnisharp.json', 'function.json')(fname) or git_root
+        vim.notify('omnisharp root_dir: fallback=' .. tostring(fallback), vim.log.levels.DEBUG)
+        on_dir(fallback)
     end,
 }
